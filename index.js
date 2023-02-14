e6d80552ead384969c33b202664aef7860ac0d9b
pussde <pussde@gmail.com>
2023-02-15 00:55:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,62 @@
+require('dotenv').config();
+
 const express = require('express');
 const line = require('@line/bot-sdk');
-const { pipeline } = require("@vitalets/google-translate-api");
-const { GPT } = require('gpt-3');
-const app = express();
+const { Configuration, OpenAIApi } = require("openai");
 
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+
+// create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET
+  channelSecret: process.env.CHANNEL_SECRET,
 };
 
+// create LINE SDK client
 const client = new line.Client(config);
 
-const gpt = new GPT({
-  apiKey: process.env.GPT_SECRET,
-  model: 'text-davinci-002',
-  temperature: 0.7
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.post('/webhook', line.middleware(config), async (req, res) => {
-  const events = req.body.events;
-
-  for (let i = 0; i < events.length; i++) {
-    const event = events[i];
-
-    if (event.type !== 'message' || event.message.type !== 'text') {
-      continue;
-    }
-
-    if (event.message.text.startsWith('/gpt')) {
-      const prompt = event.message.text.slice(4);
-      const response = await gpt.complete(prompt);
-      const reply = response.data.choices[0].text;
-      await client.replyMessage(event.replyToken, { type: 'text', text: reply });
-    }
+// event handler
+async function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
   }
 
-  res.status(200).send('OK');
-});
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: event.message.text ,
+    max_tokens: 500,
+  });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server started');
+  // create a echoing text message
+  const echo = { type: 'text', text: completion.data.choices[0].text.trim() };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
 });
